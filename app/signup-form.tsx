24021fb74c82b6846b12102475bcf45cbd6ee542
pkjha1 <prashantcool28@gmail.com>
2025-01-17 02:21:26 +0530

    Update signup-form.tsx

--- app/signup-form.tsx
+++ app/signup-form.tsx
@@ -10,7 +10,7 @@ export function SignUpForm() {
     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
       <div className="flex flex-col space-y-2 text-center">
         <h1 className="text-2xl font-semibold tracking-tight">
-          Try SaaStart for Free
+          Try Quzones for Free
         </h1>
         <p className="text-sm text-muted-foreground">
           Enter your email address to sign up and create a new organization for you and your collaborators.
