357a67abcbd9d9012bb89d784dbf3da76f122c50
pkjha1 <prashantcool28@gmail.com>
2025-01-17 03:25:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,8 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Quzones | Dimechain OÜ",
   description:
-    "Where Innovation meets Computation: new URL("https://dimechain.eu"),
+    "
+Unleash the Power of Quantum Computing: Where Innovation meets Computation"
 }
 
 export default async function RootLayout({
